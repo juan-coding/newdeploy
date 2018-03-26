4db7f3d3301eda017099b774320683b85a312582
Juan Li <juanli@MacBook-Pro-van-Juan.local>
2018-03-26 17:51:20 +0200

    add deploy,

---
+++ mysite/deploy.py
@@ -0,0 +1,24 @@
+"""
+WSGI config for mysite project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
+"""
+
+import os
+import sys
+
+
+from django.core.wsgi import get_wsgi_application
+from whitenoise.django import DjangoWhiteNoise
+
+path = "/home/likyeer/deploying_realjuanli_django/newdeploy/mysite"
+if path not in sys.path:
+    sys.path.append(path)
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
+
+application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
