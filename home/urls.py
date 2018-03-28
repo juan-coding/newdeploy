ca02e055de0e2035112427e2c70d105bb735f18b
Juan Li <juanli@MacBook-Pro-van-Juan.local>
2018-03-28 19:29:02 +0200

    add stuff to homepage

--- home/urls.py
+++ home/urls.py
@@ -1,4 +1,4 @@
-"""aboutjuanli_v1 URL Configuration
+"""realjuanli URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.0/topics/http/urls/
